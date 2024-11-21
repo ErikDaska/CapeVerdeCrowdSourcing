98ec69322ad7348c66e615288244c6e864c7f5d5
Erik Daskalyuk <erik.daska@gmail.com>
2024-11-21 18:02:37 +0000

    Crowd Sourcing Cape Verde v0.0.3 ~ Design half developed

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Phrases(models.Model):
+    portuguesePhrase = models.CharField(max_length=1000)
+    translatedPhrase = models.CharField(max_length=1000)
--- frontend/src/pages/TranslationPage.js
+++ frontend/src/pages/TranslationPage.js
@@ -1,21 +1,58 @@
 import styles from './TranslationPage.css';
+import React, { useState } from "react";
+
+
+// Alterar isto para um ficheiro.json
+const frasesAleatorias = [
+  "How are you?",
+  "Good morning!",
+  "What is your name?",
+  "Where are you from?",
+  "I love Cape Verde!"
+];
+
+
 export default function TranslationPage() {
-    const randomPhrase = "Ola mundo"
+    const [frase, setFrase] = useState("");
+  const [traducao, setTraducao] = useState("");
 
+  // Função para gerar uma frase aleatória
+  const gerarFrase = () => {
+    const randomIndex = Math.floor(Math.random() * frasesAleatorias.length);
+    setFrase(frasesAleatorias[randomIndex]);
+    setTraducao("");
+  };
 
-    return (
-      <div className={styles.webPage}>
-          <h1>Translation Page</h1>
-          <p> Este website tem como objetivo, recolher corpora de forma a treinar um modelo de língua para crioulo de
-              Cabo Verde</p>
+  // Função para atualizar a tradução
+  const handleTraducaoChange = (event) => {
+    setTraducao(event.target.value);
+  };
 
-          <form>
-              <label>
-                  {randomPhrase}
-                  <input type="text" name="frase"/>
-              </label>
-              <input type="submit" value="Submit"/>
-          </form>
-      </div>
+  const handleSubmit = () => {
+    alert(`Obrigado pelo seu contributo`);
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Traduza para o crioulo de Cabo Verde</h1>
+      </header>
+      <main>
+        <div className="frase-container">
+          <button onClick={gerarFrase}>Gerar frase</button>
+          {frase && <p className="frase">Frase: "{frase}"</p>}
+        </div>
+        {frase && (
+          <div className="traducao-container">
+            <textarea
+              placeholder="Escreva sua tradução aqui..."
+              value={traducao}
+              onChange={handleTraducaoChange}
+            />
+            <button onClick={handleSubmit}>Enviar Tradução</button>
+          </div>
+        )}
+      </main>
+    </div>
   );
 }
